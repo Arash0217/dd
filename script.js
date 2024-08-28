65225b5a6129cc41b4c0d1b49b8a003bedc94ab8
Arash0312 <arashm7100@gmail.com>
2024-08-28 16:32:57 +0330

    Update script.js
    Signed-off-by: Arash0312 <arashm7100@gmail.com>

--- script.js
+++ script.js
@@ -5,28 +5,28 @@ document.addEventListener('DOMContentLoaded', () => {
             appToken: 'd28721be-fd2d-4b45-869e-9f253b554e50',
             promoId: '43e35910-c168-4634-ad4f-52fd764a843f',
             timing: 21000, // 21 seconds
-            attempts: 22,
+            attempts: 23,
         },
         2: {
             name: 'Chain Cube 2048',
             appToken: 'd1690a07-3780-4068-810f-9b5bbf2931b2',
             promoId: 'b4170868-cef0-424f-8eb9-be0622e8e8e3',
             timing: 20000, // 20 seconds
-            attempts: 10,
-        },
-        3: {
-            name: 'My Clone Army',
-            appToken: '74ee0b5b-775e-4bee-974f-63e7f4d5bacb',
-            promoId: 'fe693b26-b342-4159-8808-15e3ff7f8767',
-            timing: 120000, // 120 seconds
             attempts: 12,
+       },
+        3: {
+            name: 'Cafe Dash',
+            appToken: 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11',
+            promoId: 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11',
+            timing: 23000, // 23 seconds
+            attempts: 14,
         },
         4: {
             name: 'Train Miner',
             appToken: '82647f43-3f87-402d-88dd-09a90025313f',
             promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
             timing: 20000, // 20 seconds
-            attempts: 10,
+            attempts: 12,
         },
         5: {
             name: 'Merge Away',
